34a7f1849c3e67a71f776de91385f355963b1d45
Mesay <mesaylema21@gmail.com>
2024-12-11 15:57:07 +0300

    check cors

--- src/main/java/EFood/controllers/AuthController.java
+++ src/main/java/EFood/controllers/AuthController.java
@@ -2,6 +2,7 @@ package EFood.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class AuthController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody signUpPayload user) {
         try {
