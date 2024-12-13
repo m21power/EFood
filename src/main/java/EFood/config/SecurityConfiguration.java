0be33d3f83412c208a057e1f5876f436cfb7df19
Mesay <mesaylema21@gmail.com>
2024-12-13 18:48:20 +0300

    optionis

--- src/main/java/EFood/config/SecurityConfiguration.java
+++ src/main/java/EFood/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package EFood.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.web.cors.CorsConfiguration;
@@ -31,6 +32,8 @@ public class SecurityConfiguration {
                 http.csrf(csrf -> csrf.disable())
                                 .cors(c -> c.configurationSource(corsConfigurationSource()))
                                 .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                                                 .requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**",
                                                                 "/swagger-ui.html")
                                                 .permitAll() // Public endpoints
@@ -62,7 +65,7 @@ public class SecurityConfiguration {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowedOrigins(
                                 List.of("https://localhost:3000"));
-                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of(
                                 "Authorization",
                                 "Content-Type",
